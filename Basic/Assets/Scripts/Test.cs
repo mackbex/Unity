1d89fe30da959dbd26ed02b508735f0e1934b7a1
Mackbex <mackbex1@gmail.com>
2022-01-02 23:52:12 +0900

    added basic unity feature

---
+++ Basic/Assets/Scripts/Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private BoxCollider col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.transform.position += new Vector3(0, 0, 1) * Time.deltaTime;
+
+        Debug.Log(other.transform.position);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.position = new Vector3(0, 2, 0);
+    }
+}
